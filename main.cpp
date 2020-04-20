86ac9d48a978fe2f90ba01a6dd05ad2de133d8ef
Denis <brenkov_d@inbox.ru>
2020-04-21 00:38:20 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "version.h"
+// #include <iostream>
+
+
+#include <chrono>
+#include <thread>
+#include <iostream>
+// #include <ncurses.h> //подключаем библиотеку ncurses
+using namespace std;
+
+int main()
+{ // инициализация (должна быть выполнена
+  cout << "Hello from ARM RaspberryPI" << endl;
+  
+  return 0;
+}
